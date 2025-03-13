8877461ff400b940fbcda5129988ccea7514785d
RuichaoTang <tangrich@outlook.com>
2025-03-13 14:49:45 -0700

    pages & component updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import PriceTier from './components/PriceTier'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import User from './pages/User';
+import Search from './pages/Search';
+import Service from './pages/Service';
+import PrivateRoute from './components/PrivateRoute.jsx';
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <PriceTier></PriceTier>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 className='text-lg bg-amber-200 underline'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/about' element={<About/>}></Route>
+          <Route path='/login' element={<Login/>} />
+          <Route path='/register' element={<Register/>} />
+          <Route path='/Search' element={<Search />} />
+          <Route path='/Service' element={<Service/>}/>
+          <Route element={<PrivateRoute/>}>
+            <Route path='/user/:userId' element={<User />} />
+
+          </Route>
+        </Routes>
+      </Router>
     </>
   )
 }
