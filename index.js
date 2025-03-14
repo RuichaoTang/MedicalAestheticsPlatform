0e1de76218e29d7465557e2b143b56b8c7ddcf27
RuichaoTang <tangrich@outlook.com>
2025-03-14 02:14:55 -0700

    file path undated

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const app = express();
 const port = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
