f781e55fdfe90ff37eb2452110f9007d357b42e8
RuichaoTang <tangrich@outlook.com>
2025-03-14 02:10:28 -0700

    connected express

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
 });
 
 app.listen(port, () => {
